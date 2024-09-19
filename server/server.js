040272319ea1a2eb8270df2a664e3e9f918fa9cf
Yogesh <yogeshkumar86077@gmail.com>
2024-09-19 11:53:19 +0530

    server

--- server/server.js
+++ server/server.js
@@ -85,8 +85,11 @@ const app = express();
 
 
 
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin: ['https://website.saumiccraft.com'], // Allow your frontend domain
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Specify allowed methods
+  allowedHeaders: ['Content-Type', 'Authorization'],  // Allow specific headers
+}));app.use(express.json());
 app.use(bodyParser.json());
 
 app.use('/api/managers', managerRoutes);
